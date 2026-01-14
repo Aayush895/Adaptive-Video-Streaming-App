45c37c2744c66525909dfeda46d6303894b2a550
Aayush895 <aayush895@gmail.com>
2026-01-14 23:58:31 +0530

    Initial typescript server setup is done

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express, { type Express, type Request, type Response } from 'express'
+
+const app: Express = express()
+
+app.get('/ping', (_req: Request, res: Response) => {
+  return res.json({
+    message: 'Pong',
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
